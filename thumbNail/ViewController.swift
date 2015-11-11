4020f6371fe10fe066854321cc001b79fb2b5436
Michael <michael.christian.frick@gmail.com>
2015-11-11 18:21:39 +0000

    swift 2.1 and some minor improvements

--- thumbNail/ViewController.swift
+++ thumbNail/ViewController.swift
@@ -13,76 +13,135 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.whiteColor()
         
         var result:UIImage?
         
-        let width:CGFloat = 10.0
-        let height:CGFloat = 20.0
+        var width:CGFloat = 50.0
+        var height:CGFloat = 200.0
         
-        let path = NSBundle.mainBundle().pathForResource("test", ofType: "png")
-        if let imageSource = CGImageSourceCreateWithURL(NSURL(fileURLWithPath: path!), nil) {
-            result = createThumbnail(imageSource, thumbWidth: width, thumbHeight: height)
-        }
         
-        if result != nil {
-            println("width: \(result!.size.width/UIScreen.mainScreen().scale)")
-            println("height: \(result!.size.height/UIScreen.mainScreen().scale)")
+        let resourse = NSBundle.mainBundle().pathForResource("test", ofType: "png")
+        let fileURL = NSURL(fileURLWithPath: resourse!)
+
+        result = thumbnailFromUrl(fileURL, thumbWidth: width, thumbHeight: height)
             
+        if result != nil {
             let iv = UIImageView(frame: CGRect(x: 10, y: 10, width: width, height: height))
             iv.image = result
             self.view.addSubview(iv)
         }
+
+        
+        width = 50.0
+        height = 200.0
+
+        if let imageSource = CGImageSourceCreateWithURL(NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("test", ofType: "png")!), nil) {
+            
+            result = createThumbnail(imageSource, thumbWidth: width, thumbHeight: height)
+            
+            if result != nil {
+                
+                
+                
+                let iv2 = UIImageView(frame: CGRect(x: 10, y: 300, width: width, height: height))
+                iv2.image = result
+                self.view.addSubview(iv2)
+            }
+        }
+        else {
+            print("WHAAAAAAT")
+        }
+    }
+    
+    func thumbnailFromUrl(filePath: NSURL, thumbWidth: CGFloat, thumbHeight: CGFloat) ->UIImage? {
+        if let imageSource = CGImageSourceCreateWithURL(filePath, nil) {
+            if let thumb = createThumbnail(imageSource, thumbWidth: thumbWidth, thumbHeight: thumbHeight) {
+                return thumb
+            }
+            else {
+                return nil
+            }
+        }
+        else {
+            return nil
+        }
+        
     }
     
     func createThumbnail(imageSource:CGImageSource, thumbWidth: CGFloat, thumbHeight: CGFloat) ->UIImage? {
-        let thumbImage:CGImage!
         let thumbUI:UIImage!
-        
         let scale = UIScreen.mainScreen().scale
+        
         var maxSize:CGFloat = 0.0
-        var crop = false
+        var orientationPortrait:Bool {return thumbHeight >= thumbWidth}
+        var newWidth = thumbWidth * scale
+        var newHeight = thumbHeight * scale
         
         // gets the size from the imageSource
-        let origSize:CGSize = getIMGSourceSize(imageSource)
+        let origSize:CGSize = getImgSourceSize(imageSource)!
+        let aspectRatio = origSize.width/origSize.height
         
-        if origSize.width >= thumbWidth * scale && origSize.height >= thumbHeight * scale {
-            
-            let cropRect:CGRect!
-            
-            if origSize.width > origSize.height{
-                maxSize = origSize.width/origSize.height * thumbHeight
-                //crop = true
-                cropRect = CGRect(x: (maxSize - thumbWidth)/2, y: 0, width: thumbWidth * scale, height: thumbHeight * scale)
+        if orientationPortrait {
+            if thumbHeight * scale > origSize.height {
+                newHeight = origSize.height
+                newWidth = newHeight * thumbWidth/thumbHeight
             }
-            else {
-                maxSize = origSize.height/origSize.width * thumbHeight
-                
-                cropRect = CGRect(x: 0.0, y: (maxSize - thumbHeight)/2, width: thumbWidth * scale, height: thumbHeight * scale)
-            }
-            
-            let options = [
-                kCGImageSourceShouldAllowFloat as String: true,
-                kCGImageSourceCreateThumbnailFromImageAlways as String: true,
-                kCGImageSourceThumbnailMaxPixelSize as String : maxSize * scale,
-                kCGImageSourceCreateThumbnailFromImageIfAbsent as String : true
-            ]
-            thumbImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, options)
-            
-            return UIImage(CGImage: CGImageCreateWithImageInRect(thumbImage!, cropRect)!)!
         }
         else {
-            NSLog("You can't create a thumbnail which is bigger than the original image")
-            return nil
+            if thumbWidth * scale > origSize.width {
+                newWidth = origSize.width
+                newHeight = newWidth/(thumbWidth/thumbHeight)
+            }
         }
+        
+        if origSize.width > origSize.height {
+            maxSize = orientationPortrait ? newHeight * aspectRatio : newWidth
+        }
+        else {
+            maxSize = orientationPortrait ? newHeight : newHeight * aspectRatio
+        }
+
+        let options = [
+            kCGImageSourceShouldAllowFloat as String: true,
+            kCGImageSourceCreateThumbnailFromImageAlways as String: true,
+            kCGImageSourceThumbnailMaxPixelSize as String : maxSize,
+            kCGImageSourceCreateThumbnailFromImageIfAbsent as String : true,
+            kCGImageSourceCreateThumbnailWithTransform as String :true
+        ]
+        
+        if let thumbImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, options) {
+            let newSize = UIImage(CGImage: thumbImage).size
+            
+            if orientationPortrait {
+                let cropRect = CGRect(x: (newSize.width-newWidth)/2, y: 0.0, width: newWidth, height: newHeight)
+                print(cropRect)
+                return UIImage(CGImage: CGImageCreateWithImageInRect(thumbImage, cropRect)!)
+            }
+            else {
+                let cropRect = CGRect(x: 0.0, y: (newSize.height-newHeight)/2, width: newWidth, height: newHeight)
+                print(cropRect)
+                return UIImage(CGImage: CGImageCreateWithImageInRect(thumbImage, cropRect)!)            }
+        }
+        return nil
     }
     
-    func getIMGSourceSize(imageSource: CGImageSource) ->CGSize {
-        let imageProperties = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, nil) as Dictionary
-        let width = imageProperties[kCGImagePropertyPixelWidth] as! NSNumber
-        let height = imageProperties[kCGImagePropertyPixelHeight] as! NSNumber
-        
-        let size = CGSize(width: CGFloat(width), height: CGFloat(height))
-        return size
+    private func getImgSourceSize(imageSource: CGImageSource) ->CGSize? {
+        if let imgProps = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, nil) as Dictionary? {
+            let height = imgProps[kCGImagePropertyPixelHeight as String!] as! Int!
+            let width = imgProps[kCGImagePropertyPixelWidth as String!] as! Int!
+            
+            if height == nil || width == nil {
+                return CGSize(width: UIScreen.mainScreen().bounds.width , height: UIScreen.mainScreen().bounds.height)
+            }
+            else {
+                return CGSize(width: width, height: height)
+            }
+        }
+        else {
+            return CGSize(width: UIScreen.mainScreen().bounds.width, height: UIScreen.mainScreen().bounds.height)
+        }
     }
+
 }
 
